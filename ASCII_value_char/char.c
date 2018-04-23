4ee1cd3e3bc02fbcc81d94e63026d57fa45118a2
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2018-04-23 07:03:10 +0530

    Create char.c

---
+++ ASCII_value_char/char.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+// if you enter any character in this program the program give the ASCII value of this character. 
+void main(void)
+{
+  char ch;
+  clrscr();
+  printf("\t enter the character\t");
+  scanf("%d",&ch);
+  printf("\n%d\t");
+  getch();
+}
+/* output 
+  enter the character   a
+  97
+  */
