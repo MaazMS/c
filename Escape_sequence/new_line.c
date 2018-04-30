a5b71aa965f2a21d43f800063be2365f3c0ffdc0
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2018-04-30 19:12:31 +0530

    Create new_line.c

---
+++ Escape_sequence/new_line.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main(void)
+//In this program use "\n". It is use for new line.
+{
+   clrscr();
+   printf("welcome");
+   printf("\n hello");
+   getch();
+}
+/* output
+welcome
+ hello.  */
