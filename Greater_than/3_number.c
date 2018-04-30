eec2ea7f70ffbabbe7d35240487a18ae10a20182
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2018-04-30 16:19:19 +0530

    Update 3_number.c

--- Greater_than/3_number.c
+++ Greater_than/3_number.c
@@ -1,1 +1,27 @@
-
+#include<stdio.h>
+#inclide<conio.h>
+void main(void)
+// in this program to check which number is greater.
+// First number is check by second number and third number.
+// second number is check by third number .
+// then finally use else statement.
+ {
+      int a,b,c;
+        clrscr();
+      printf("enter the three number");
+      scanf("%d\t%d\t%d\t",&a,&b,&c);
+      if((a>b) && (a>c))
+       { 
+           printf("\n a is greater ");
+        }
+       else if(b>c)
+         {
+           printf("\n b is greater ");
+          }
+       else 
+         {
+            printf("\n c is greater ");
+         {
+         getch();
+  }
+           
