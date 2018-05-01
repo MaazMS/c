1b86f4086a158ee4f1847b223183dee43a662f2f
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2018-05-01 22:22:12 +0530

    Create Tab.c

---
+++ Escape_sequence/Tab.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void mani()
+/* The \t is a escape sequence.
+It is use to tab after the characters, numbers etc.
+A one tab has 8 space. */
+{
+  clrscr();
+  printf("welcome \t back");
+  getch();
+}
+/* output 
+welcome          back
+*/
