818c73e38ab937ae8894a3ebb9dd94ad52a42c65
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-01-20 00:21:34 -0800

    Update Armstrong.c

--- Armstrong/Armstrong.c
+++ Armstrong/Armstrong.c
@@ -1,22 +1,46 @@
-#include<stdio.h> 
-#include<conio.h>
-void main(void) 
+
+/*
+  A whole number, or integer, that is the sum of the nth powers of its digits  is called an Armstrong number. It is also sometimes
+  known as an n-narcissistic number , perfect digital invariant ,plus perfect number .if you find three digit Armstrong number then 
+  multiply remainder three times and if you find four digit armstrong number then remainder multiply four times .
+  Armstrong number source:http://oeis.org/A005188
+*/
+
+
+#include<stdio.h>
+
+int main(void)
 {
-       int number,sum=0,addition,remainder=0,increase=0;
-       clrscr(); 
-       printf("enter the number");
-       scanf("%d",&number); 
-       addition=number;
-       while(number>0) 
+       int number=0,sum=0,Same_multiply=0,addition=0,remainder=0,increase=0;
+       setbuf(stdout, NULL);
+
+       printf("enter the value of number ");
+       scanf("%d",&number);                                                   // user enter the any digit in 'number'
+       addition=number;                                                      //Assign 'number' variable value  into 'addition' variable
+
+       while(number>0)
        {
-              remainder=number%10;
-              number=number/10; 
-              sum=remainder*remainder*remainder; 
-              increase=increase+1;
+           remainder=number%10;                                                 // To get last digit of number use mode operation ( % ) .
+           Same_multiply=remainder*remainder*remainder*remainder*remainder;     // The last digit number multiply by the same number.
+           sum=sum+Same_multiply;                                               // sum is collect all value for armstrong number
+           number=number/10;                                                     /* if last number is finished then check second last  
+                                                                                  number until the first number .*/
+           increase=increase+1;  	                                                // increase variable use to continue the while loop
+       }
+
+        if(sum==addition)                                                          /* compare the 'addition' variable ( which assign by
+                                                                                      'number variable) with 'sum' variable */
+        {
+           printf("number is Armstrong");
         }
-        if(sum==addition)
-             printf("number is Armstrong"); 
         else
-             printf("number is not Armstrong"); 
-         getch();   
-}            
+        {
+           printf("number is not Armstrong");
+        }
+
+}
+/*
+output
+enter the value of number 1634
+number is Armstrong 
+*/
