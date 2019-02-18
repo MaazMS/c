69e4ae319acae26734d4f27d55d28f029a1915c8
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-02-18 15:47:35 -0800

    Create character_datatype1.c

---
+++ Datatype/character_datatype/character_datatype1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+
+{
+	 
+  char character='M';		//  initialization and declaration of character .
+  
+
+	printf("character is %c \n ",character);
+	printf("enter the character ");
+	
+	
+	scanf("%c",&character);		// initialization  of character .
+  
+	printf("you have typed character is %c \n ",character);
+
+	return 0;
+}
+
+/*
+OUTPUT
+character is M 
+ enter the character m
+you have typed character is m 
+ 
+*/
