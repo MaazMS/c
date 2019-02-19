5b1aa1911f7b7e5228bdeddd594293ce56379c6e
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-02-18 16:23:59 -0800

    Create ASCII_value_Uppercase_characters.c

---
+++ ASCII_value_char/ASCII_value_Uppercase_characters.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+int main(void)
+{
+	
+	int number=65;
+
+	printf("enter the number\n");
+
+	for(number=65;number<=90;number++)
+	printf("%d - %c \n ",number,number);			// return  ASCII value number by their character .
+	
+	return 0;
+
+}
+/*
+ OUTPUT
+
+ enter the number
+65 - A 
+ 66 - B 
+ 67 - C 
+ 68 - D 
+ 69 - E 
+ 70 - F 
+ 71 - G 
+ 72 - H 
+ 73 - I 
+ 74 - J 
+ 75 - K 
+ 76 - L 
+ 77 - M 
+ 78 - N 
+ 79 - O 
+ 80 - P 
+ 81 - Q 
+ 82 - R 
+ 83 - S 
+ 84 - T 
+ 85 - U 
+ 86 - V 
+ 87 - W 
+ 88 - X 
+ 89 - Y 
+ 90 - Z 
+ 
+*/
