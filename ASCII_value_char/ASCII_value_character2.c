2e3b9c8a6052b699e912ef04e11eb18e6abd420b
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-02-18 16:15:37 -0800

    Create ASCII_value_character2.c

---
+++ ASCII_value_char/ASCII_value_character2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(void)
+{
+
+
+	int number;
+
+	printf("enter the number\n");
+
+	scanf("%d",&number);
+	printf("%d - %c ",number,number);			// return number and  ASCII value  character of that number .
+  
+  return 0 ;
+  
+}
+/*
+ OUTPUT
+ 
+enter the number
+109
+109 - m 
+
+*/
