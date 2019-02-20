b0cf4d37eecec68e93df3bf8d624158b2897512a
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-02-19 21:25:17 -0800

    Create printf2.c

---
+++ function_printf/printf/printf2.c
@@ -0,0 +1,30 @@
+/* 
+ * First printf print the string and return the size of string to integer variable .
+ * The integer variable print the size of string by integer format. 
+*/
+
+
+#include <stdio.h>
+
+
+int  main()
+{
+
+  int integer;
+ 
+ 
+	integer=printf("Hello");  // print the string and return the size of string
+  
+	printf("%d",integer);			// integer variable print the size of string by integer format.
+	
+  
+  return 0 ;
+ 
+ }
+ 
+ /*
+ OUTPUT
+ 
+ Hello5
+ 
+ */
