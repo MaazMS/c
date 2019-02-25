9ce49af0cbafd38d9a6683c7f3f01ed769c7f6ca
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-02-25 07:55:11 -0800

    Create string4.c

---
+++ Datatype/string_data_type/string4.c
@@ -0,0 +1,23 @@
+//if use  gets function it is not cut extra character  after space and enter .
+	
+#include<stdio.h>
+
+int main()
+
+{
+	
+	char string[10];
+
+	printf("Enter the character for string");
+	
+  gets(string);  // accept string .       
+
+	printf("%s",string);
+	return 0 ;
+}
+
+/*
+ OUTPUT
+Enter the character for stringhellow world
+hellow world
+ */
