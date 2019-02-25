7fb29e733c696cb72d271948363bba35ae5d1b41
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-02-25 07:45:50 -0800

    Create string3.c

---
+++ Datatype/string_data_type/string3.c
@@ -0,0 +1,20 @@
+// declaration of string at compile time  
+
+#include<stdio.h>
+
+int main()
+
+{
+	
+  char string[]="hello world"; 		// initialization of string without string  
+	
+	printf("%s",string);
+  
+	return 0 ;
+  
+}
+/*
+OUPUT
+Enter the character for stringhellow world
+hellow world
+ */
