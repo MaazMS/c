626ac32d6a22dfc58e34fd6bf17bb5c0a859f25e
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-02-25 07:36:17 -0800

    Create string_using_scanf_1.c

---
+++ Datatype/string_data_type/string_using_scanf_1.c
@@ -0,0 +1,25 @@
+
+
+#include<stdio.h>
+
+int main()
+
+{
+	char string[10];     // Declaration of string using data type char and size defined by square bracket [] 
+  
+  
+	printf("Enter the character for string");
+	
+  scanf("%s",string);                                 // accept string 
+	
+  printf("%s",string);                               // print string using format specifier %s
+	
+  
+  return 0 ;
+  
+}
+/*
+OUTPUT
+Enter the character for stringhellow world
+hellow
+ */
