5d25d16f420d0df55f1cfde1d747909bf861372e
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-06-14 23:40:54 -0700

    change the sequence of line

--- Scanset/Scanset use for N line/Scanset use for many line.c
+++ Scanset/Scanset use for N line/Scanset use for many line.c
@@ -7,14 +7,14 @@
  * 1) if use two symbol to after bitwise exclusive  operator then write to symbol together example @#.
  * 
  * check some example with syntax and explination .
- * scanf("%[^a]",string);             If type  'a' as input then  it give output.
- * scanf("%[^A]",string);	            If type  'A' as input then  it give output.
+ * scanf("%[^a]",string);                 If type  'a' as input then  it give output.
+ * scanf("%[^A]",string);	          If type  'A' as input then  it give output.
  * scanf("%[^a-z]",string);	          If type  'a-z' as input then  it give output.
  * scanf("%[^A-Z]",string);	          If type  'A-Z' as input then  it give output.
  * scanf("%[^0-9]",string);	          If type  '0-9' as input then  it give output.
  * scanf("%[^A-Z-0-9-a-z]",string);	  If type  'A-Z-0-9-a-z' as input then  it give output.
- * scanf("%[^@#]",string);			      If type  '@#' as input then  it give output.
- * scanf("%[^@,#]",string);           If type  '@,#' as input then  it give output.
+ * scanf("%[^@#]",string);                If type  '@#' as input then  it give output.
+ * scanf("%[^@,#]",string);               If type  '@,#' as input then  it give output.
  * 
  */
 #include<stdio.h>
@@ -22,13 +22,13 @@
 int main()
 
 {
-	 char string[90];
+   char string[90];
    
    scanf("%[^$]",string);     // If type  '$' as input then  it give output.
 	
-	 printf("%s",string);
+   printf("%s",string);
 
-	 return 0;
+   return 0;
 
 }
 
