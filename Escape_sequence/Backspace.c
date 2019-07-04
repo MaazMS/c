4cc71eeaca370046321cd35526dcf7978b4f0e3c
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-07-04 10:11:52 -0700

    Update and rename Back.c to Backspace.c

---
+++ Escape_sequence/Backspace.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main(void)
+// the \b is a escape sequence.
+// Move the cursor back one position on the current line.
+
+{
+    
+      printf("Md Maaz\b\b\bshaikh");
+    
+}
+/* output 
+MdMshaikh 
+*/
