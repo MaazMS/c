06fffeef5ec00384c0ef436ea5650513619e1ca9
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-07-05 08:22:24 -0700

    Create Single_Quote.c

---
+++ Escape_sequence/Single_Quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>  
+
+int main()
+{
+	
+   printf("\' Hello World \'");  //Single quote with escape squence use to print single quote with output.
+	
+   return 0;
+}
+/* OUTPUT
+' Hello World '
+*/
