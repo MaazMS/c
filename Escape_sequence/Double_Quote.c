957a6f7b5a9d451ab4e81df67daf4246ad716edd
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-07-05 08:25:42 -0700

    Create Double_Quote.c

---
+++ Escape_sequence/Double_Quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+	printf("\" Hello World \"");   // Double quote is print in output with the help of escape sequence double quote.
+	
+   	return 0;
+}
+
+/*OUTPUT
+" Hello World "
+*/
