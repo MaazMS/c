cb88f16edc046d7ca167fe7a7ca0078df58781d7
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-11-25 23:15:30 -0800

    Update and rename Febonacci.c to Febonacci/Febonacci.c

--- Febonacci/Febonacci.c
+++ Febonacci/Febonacci.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
-#include<conio.h>
+
 void main() 
 { 
         int number,first=0,second=1,third,increase=2;
-        clrscr(); 
+        
         printf("enter the number"); 
         scanf("%d",&number);
+        
         printf("%d%d",first,second); 
-        while(increase<number) 
+        
+        while(increase<number)   // increase start form 2 because 0 and 1 is already print. 
         {
              third=first+second;
              first=second;
@@ -15,5 +17,5 @@ void main()
              increase=increase+1;
              printf("%d",third);
         }
-        getch();
+        
 }
