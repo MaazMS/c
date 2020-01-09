b2ffe8214c82bd0621e939d4659fc0abeb0dade0
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2020-01-08 22:22:35 -0800

    Update Array_initialization_2.c

--- Array/Array_initialization/Array_initialization_2.c
+++ Array/Array_initialization/Array_initialization_2.c
@@ -1,28 +1,24 @@
-/*
-  An array is collection of similar data type items stored at continuous memory locations and accessed using a common name.
-*/
+//  An array is collection of similar data type items stored at continuous memory locations and accessed using a common name.
 
 #include<stdio.h>
 
 int main()
  {
-   int index,number=6;
-   int block[5]={1,2,3,4,5,6};                     /* warning: excess elements in array initializer
-                                                          Declare the array variable*/
+   int index,number=5;
+   int block[5]={1,2,3,4,5};                     
    
    printf("\n display array  the values \n ");
    for (index = 0; index < number; index++)
    {
-      printf("[%d] = %d\n", index, block[index]);   // warning: iteration 5u invokes undefined behavior [-Waggressive-loop-optimizations]
+      printf("[%d] = %d\n", index, block[index]);   
    }
    return (0);
  }
  /*
 display array  the values 
-[0] = 1
+ [0] = 1
 [1] = 2
 [2] = 3
 [3] = 4
 [4] = 5
-[5] = 0
  */
