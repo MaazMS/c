72871fc675253b07e3fc8d21ad85921ca143cbf7
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2020-01-08 22:25:00 -0800

    Update Array_initialization_3.c

--- Array/Array_initialization/Array_initialization_3.c
+++ Array/Array_initialization/Array_initialization_3.c
@@ -1,18 +1,18 @@
 /*
   An array is collection of similar data type items stored at continuous memory locations and accessed using a common name.
-  if allocated memory by array is fixd then the other memory is store zero value .for example block[index]3,4,5=zero.
+  if allocated memory by array is fixd then the other memory is store zero value .for example block[index]3,4=zero.
   */
 
 #include<stdio.h>
 
 int main()
  {
-   int index,number=6;
+   int index,number=5;
    int block[5]={1,2,3};                              //Declare the array variable. 
    printf("\n display array  the values \n ");
    for (index = 0; index < number; index++)
    {
-      printf("[%d] = %d\n", index, block[index]);     // warning: iteration 5u invokes undefined behavior [-Waggressive-loop-optimizations]
+      printf("[%d] = %d\n", index, block[index]);     
 
    }
      return (0);
@@ -24,5 +24,4 @@ int main()
 [2] = 3
 [3] = 0
 [4] = 0
-[5] = 0
 */
